b33dd3c25790d2406f4d2c840746c005f087c4c3
Su Latt Phone <sulattphone@gmail.com>
2020-06-08 03:15:41 -0700

    Added downloaded kafka bin files and just built wordcount

--- src/main/java/org/example/WordCount.java
+++ src/main/java/org/example/WordCount.java
@@ -2,12 +2,15 @@ package org.example;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.state.KeyValueStore;
 
 import java.util.Arrays;
 import java.util.Properties;
@@ -20,28 +23,36 @@ public class WordCount {
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-    }
 
-    StreamsBuilder builder = new StreamsBuilder();
-    //1. stream for kafka
-    KStream<String, String> textLines = builder.stream("word-count-input");
 
-    KTable<String, Long> wordCounts = textLines
-            // 2 - map values to lowercase
-            .mapValues(textLine -> textLine.toLowerCase())
-            // can be alternatively written as:
-            // .mapValues(String::toLowerCase)
-            // 3 - flatmap values split by space
-            .flatMapValues(textLine -> Arrays.asList(textLine.split("\\W+")))
-            // 4 - select key to apply a key (we discard the old key)
-            .selectKey((key, word) -> word)
-            // 5 - group by key before aggregation
-            .groupByKey()
-            // 6 - count occurences
-            .count(Materialized.as("Counts"));
+        StreamsBuilder builder = new StreamsBuilder();
+        //1. stream for kafka
+        KStream<String, String> textLines = builder.stream("word-count-input");
 
-    //7. to in order to write the result back to Kafka
-     wordCounts.toStream().to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
+        KTable<String, Long> wordCounts = textLines
+                // 2 - map values to lowercase
+                .mapValues(textLine -> textLine.toLowerCase())
+                // can be alternatively written as:
+                // .mapValues(String::toLowerCase)
+                // 3 - flatmap values split by space
+                .flatMapValues(textLine -> Arrays.asList(textLine.split("\\W+")))
+                // 4 - select key to apply a key (we discard the old key)
+                .selectKey((key, word) -> word)
+                // 5 - group by key before aggregation
+                .groupByKey()
+                // 6 - count occurences
+                //.count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("counts-store"));
+                .count(Materialized.as("Counts"));
 
-     return builder.build();
+        //7. to in order to write the result back to Kafka
+        wordCounts.toStream().to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+        streams.start();
+        //printed the topology
+        System.out.println(streams.toString());
+
+        //shutdown hook to correctly close the streams application
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    } 
 }
